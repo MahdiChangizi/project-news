2bc049dead2b4adeda62b4b162aee14f457ce85a
mahdi changizi <mchangizi10@gmail.com>
2023-09-25 13:58:46 +0330

    modify code

--- resources/views/home/layouts/script.blade.php
+++ resources/views/home/layouts/script.blade.php
@@ -1,7 +1,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
 <script src="{{ asset('home/js/vendor/jquery-2.2.4.min.js') }}"></script>
-<script src="{{ asset('home/js/vendor/bootstrap.min.js') }}"></script>
+<script src="{{ asset('admin/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('home/js/easing.min.js') }}"></script>
 <script src="{{ asset('home/js/hoverIntent.js') }}"></script>
 <script src="{{ asset('home/js/superfish.min.js') }}"></script>
