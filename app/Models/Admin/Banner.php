fed958b93b0444ad8c81e64833776b2dfc4fb619
mahdi changizi <mchangizi10@gmail.com>
2023-09-30 14:27:29 +0330

    delete codes

--- app/Models/Admin/Banner.php
+++ app/Models/Admin/Banner.php
@@ -5,7 +5,6 @@ namespace App\Models\Admin;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-// use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Banner extends Model
 {
