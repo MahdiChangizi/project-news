475247f058e90fd95649410730d70596ba70b1f6
mahdi changizi <mchangizi10@gmail.com>
2023-09-30 14:04:49 +0330

    dbug

--- resources/views/auth/layout/script.blade.php
+++ resources/views/auth/layout/script.blade.php
@@ -1,6 +1,5 @@
 <script src="{{ asset('auth/assets/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
 <script src="{{ asset('admin/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('auth/assets/js/main.js') }}"></script>
 
 
 
